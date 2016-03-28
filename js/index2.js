37230169f36efc4722263c5229bbe1827f203823
Emilio Núñez <emiliojes@gmail.com>
2016-03-27 19:30:47 -0500

    Changing in the css, js, html add other cities..

--- js/index2.js
+++ js/index2.js
@@ -1,10 +1,19 @@
 (function(){
+
+	var API_WORLDTIME_KEY = "9349c1debe5144dc9f6225928162703";
+	var API_WORLDTIME = "HTTPS: https://api.worldweatheronline.com/free/v2/weather.ashx?format=json&key=" + API_WORLDTIME_KEY + "&q=";
+	//https://api.worldweatheronline.com/free/v2/tz.ashx?format=json
 	var API_WEATHER_KEY = "7c5547f7bf573c56d170f8559bc6756f";
 	var API_WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?APPID=" + API_WEATHER_KEY + "&";
 	var IMG_WEATHER = "http://openweathermap.org/img/w/";
 
 	var today = new Date();
 	var timeNow = today.toLocaleTimeString();
+
+	var $body = $("body");
+	var $loader = $(".loader");
+	var nombreNuevaCiudad = $("[data-input='cityAdd']");
+	var buttonAdd = $("[data-button='add']");
 
 	var cityWeather = {};
 	cityWeather.zone;
@@ -13,6 +22,14 @@
 	cityWeather.temp_max;
 	cityWeather.temp_min;
 	cityWeather.main;
+
+	$( buttonAdd ).on("click", addNewCity);
+
+	$( nombreNuevaCiudad ).on("keypress", function(event){
+		if(event.which == 13){
+			addNewCity();
+		}
+	});
 
 	if (navigator.geolocation) {
 
@@ -45,7 +62,7 @@
 	cityWeather.temp_min = data.main.temp_min - 273.15;
 	cityWeather.main = data.weather[0].main;
 
-	renderTemplate();
+	renderTemplate(cityWeather);
 	};
 
 	function activateTemplate(id)
@@ -54,20 +71,55 @@
 		return document.importNode(t.content, true);
 	};
 
-	function renderTemplate(){
+	function renderTemplate(cityWeather, localtime){
+
 		var clone = activateTemplate("#template--city");
 
-		clone.querySelector("[data-time]").innerHTML = timeNow;
+		var timeToShow;
+		if(localtime == true){
+			timeToShow = localtime;
+		}else {
+			timeToShow = timeNow;
+		}
+
+
+		clone.querySelector("[data-time]").innerHTML = timeToShow;
 		clone.querySelector("[data-city]").innerHTML = cityWeather.zone;
 		clone.querySelector("[data-icon]").src = cityWeather.icon;
 		clone.querySelector("[data-temp='max']").innerHTML = cityWeather.temp_max.toFixed(1);
 		clone.querySelector("[data-temp='min']").innerHTML = cityWeather.temp_min.toFixed(1);
 		clone.querySelector("[data-temp='current']").innerHTML = cityWeather.temp.toFixed(1);
 		
-		$(".loader").hide();
+		$($loader).hide();
+		//$(".loader").hide();
 
-		$("body").append(clone);
+		$($body).append(clone);
 
+
+	}
+
+	function addNewCity(event) {
+		event.preventDefault();
+		$.getJSON(API_WEATHER_URL + "q=" + $(nombreNuevaCiudad).val(), getWeatherNewCity );
+	}
+
+	function getWeatherNewCity(data){
+		
+		$.getJSON(API_WORLDTIME + $(nombreNuevaCiudad).val(), function(response){
+
+		cityWeather = {};
+		cityWeather.zone = data.name;
+		cityWeather.icon = IMG_WEATHER + data.weather[0].icon + ".png";
+		cityWeather.temp = data.main.temp - 273.15;
+		cityWeather.temp_max = data.main.temp_max - 273.15;
+		cityWeather.temp_min = data.main.temp_min - 273.15;
+
+		//console.log(response);
+		renderTemplate(cityWeather, response.data.time_zone[0].localtime);
+
+		}) ;
+
+	
 	}
 
 })();
